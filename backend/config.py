3e287c7dfec97e73a4760707ecd104aa49134721
YC <ycgoh04@gmail.com>
2026-04-06 20:47:58 +0800

    Set up config file

---
+++ backend/config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    # MongoDB
+    MONGODB_USERNAME = os.getenv("MONGODB_USERNAME")
+    MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
+    MONGODB_URL = os.getenv("MONGODB_URL")
+    
+    # AWS
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_REGION = os.getenv("AWS_REGION")
+    AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+
+    # AI
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+settings = Settings()
